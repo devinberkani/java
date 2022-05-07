05aab534419bd0caa97711a59e6382d155aa7441
Devin Berkani <devinberkani@gmail.com>
2022-05-07 10:18:49 -0400

    first commit

---
+++ hello-world/src/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Devin");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+//        sout shortcut
+        System.out.println(myFirstNumber);
+    }
+}
