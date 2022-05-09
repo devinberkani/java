ca5aa00f96a636f86b80fac5d8af1d12df8035e4
Devin Berkani <devinberkani@gmail.com>
2022-05-09 14:57:39 -0400

    begin methods lesson

---
+++ Methods/src/com/example/Main.java
@@ -0,0 +1,51 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        int highScore = calculateScore(true, 800, 5, 100);
+//        System.out.println("Your final score was " + highScore);
+//
+//
+//        highScore = calculateScore(true, 10000, 8, 200);
+//        System.out.println("Your final score was " + highScore);
+
+        displayHighScorePosition("Devin", calculateHighScorePosition(1500));
+
+        displayHighScorePosition("Carrie", calculateHighScorePosition(900));
+
+        displayHighScorePosition("Cam", calculateHighScorePosition(400));
+
+        displayHighScorePosition("Nate", calculateHighScorePosition(50));
+
+    }
+
+    // must include type of variable
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+
+        if (gameOver) {
+            int finalScore = (score + (levelCompleted * bonus));
+            finalScore += 2000;
+            return finalScore;
+        }
+        return -1;
+    }
+
+    public static void displayHighScorePosition(String playersName, int highScorePosition) {
+        System.out.println(playersName + " managed to get into position " + highScorePosition + " on the high score table.");
+    }
+
+    public static int calculateHighScorePosition(int playerScore) {
+        if (playerScore >= 1000) {
+            return 1;
+        } else if (playerScore >= 500) {
+            return 2;
+        } else if (playerScore >= 100) {
+            return 3;
+        } else {
+            return 4;
+        }
+    }
+}
+
+
