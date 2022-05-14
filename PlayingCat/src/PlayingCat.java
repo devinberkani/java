d9f0481763cee5d3401638091ba8baca734ea23c
Devin Berkani <devinberkani@gmail.com>
2022-05-13 21:12:14 -0400

    complete section 3 coding exercises

---
+++ PlayingCat/src/PlayingCat.java
@@ -0,0 +1,21 @@
+public class PlayingCat {
+    public static boolean isCatPlaying(boolean summer, int temperature) {
+        if (summer) {
+            if ((temperature >=25) && (temperature <= 45)) {
+                System.out.println("true");
+                return true;
+            } else {
+                System.out.println("false");
+                return false;
+            }
+        } else {
+            if ((temperature >=25) && (temperature <= 35)) {
+                System.out.println("true");
+                return true;
+            } else {
+                System.out.println("false");
+                return false;
+            }
+        }
+    }
+}
