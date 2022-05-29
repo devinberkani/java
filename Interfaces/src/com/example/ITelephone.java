a1b6c5325a6d343c70ff9041ffbdf87f677c8473
Devin Berkani <devinberkani@gmail.com>
2022-05-29 07:00:26 -0400

    complete first lesson on interfaces

---
+++ Interfaces/src/com/example/ITelephone.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public interface ITelephone {
+
+    void powerOn();
+    void dial(int phoneNumber);
+    void answer();
+    boolean callPhone(int phoneNumber);
+    boolean isRinging();
+
+}
