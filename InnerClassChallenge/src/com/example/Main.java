7bf27fbe71604c297c54042601bc93263f3db2b6
Devin Berkani <devinberkani@gmail.com>
2022-06-01 07:08:20 -0400

    begin simple chatty bot lessons

---
+++ InnerClassChallenge/src/com/example/Main.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        //write code below
+        System.out.println(10 / (2 + 2) + 4 * 8);
+    }
+}
---
+++ Simple Chatty Bot/Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -0,0 +1,9 @@
+package bot;
+
+public class SimpleBot {
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hello! My name is Jarvis.");
+        System.out.println("I was created in 2022.");
+    }
+}
