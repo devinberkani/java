5250dcf1d6db53c14c2e2d64121f19fea23c215a
Devin Berkani <devinberkani@gmail.com>
2022-06-10 08:01:58 -0400

    complete stage 3/5 for cinema project

--- Cinema Room Manager/Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/Cinema Room Manager/task/src/cinema/Cinema.java
@@ -18,7 +18,34 @@ public class Cinema {
         int numOfSeats = scanner.nextInt();
 
         // array for initial seating arrangement
-        char[][] seatingArrangement = new char[7][8];
+        char[][] seatingArrangement = new char[numOfRows][numOfSeats];
+
+        // ***** INITIAL EMPTY SEATING PRINTOUT *****
+
+        System.out.println("Cinema:");
+
+        // fill the array
+        for (int i = 0; i < seatingArrangement.length; i++) {
+            for (int j = 0; j < seatingArrangement[i].length; j++) {
+                seatingArrangement[i][j] = 'S';
+            }
+        }
+
+        // top line of seating arrangement printout
+        System.out.print("  "); // initial spaces for alignment
+        for (int i = 1; i <= seatingArrangement[0].length; i++) {
+            System.out.print(i + " ");
+        }
+
+        // rest of seating arrangement printout
+        System.out.println(); //initial new line
+        for (int i = 0; i < seatingArrangement.length; i++) {
+            System.out.print(i + 1 + " ");
+            for (int j = 0; j < seatingArrangement[i].length; j++) {
+                System.out.print(seatingArrangement[i][j] + " ");
+            }
+            System.out.println();
+        }
 
         // calculate and print profit
         int totalSeats = numOfRows * numOfSeats;
@@ -43,17 +70,45 @@ public class Cinema {
             }
         }
 
-        System.out.println("Total income:");
-        System.out.println("$" + totalProfit);
+        // print total income if all seats are sold out
+//        System.out.println("Total income:");
+//        System.out.println("$" + totalProfit);
+
+        // prompt user for row and seat numbers
+
+        System.out.println("Enter a row number:");
+
+        int userRowNumber = scanner.nextInt();
+
+        System.out.println("Enter a seat number in that row:");
+
+        int userSeatNumber = scanner.nextInt();
+
+        // ticket price for this selection
+
+        int individualTicketPrice = 10;
+        int frontHalfRows = 0;
+        int backHalfRows = 0;
+
+        if (numOfRows % 2 == 0) {
+            frontHalfRows = (numOfRows / 2);
+            backHalfRows = (numOfRows / 2);
+        } else {
+            frontHalfRows = (numOfRows / 2);
+            backHalfRows = numOfRows - frontHalfRows;
+        }
+
+        if (userRowNumber <= 4) {
+            individualTicketPrice = 10;
+        } else if (userRowNumber >= backHalfRows) {
+            individualTicketPrice = 8;
+        }
+
+        System.out.println("Ticket price: $" + individualTicketPrice);
+
+        // ***** PRINTOUT AFTER USER SEAT SELECTION *****
 
         System.out.println("Cinema:");
-
-        // fill the array
-        for (int i = 0; i < seatingArrangement.length; i++) {
-            for (int j = 0; j < seatingArrangement[i].length; j++) {
-                seatingArrangement[i][j] = 'S';
-            }
-        }
 
         // top line of seating arrangement printout
         System.out.print("  "); // initial spaces for alignment
@@ -66,6 +121,9 @@ public class Cinema {
         for (int i = 0; i < seatingArrangement.length; i++) {
             System.out.print(i + 1 + " ");
             for (int j = 0; j < seatingArrangement[i].length; j++) {
+                if (userRowNumber - 1 == i && userSeatNumber - 1 == j) {
+                    seatingArrangement[i][j] = 'B';
+                }
                 System.out.print(seatingArrangement[i][j] + " ");
             }
             System.out.println();
