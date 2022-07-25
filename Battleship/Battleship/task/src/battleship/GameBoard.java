c4297b683f3d2eb9ae3298e111c2e86de9891554
Devin Berkani <devinberkani@gmail.com>
2022-07-25 09:10:18 -0400

    implement ability to set game pieces in battleship

--- Battleship/Battleship/task/src/battleship/GameBoard.java
+++ Battleship/Battleship/task/src/battleship/GameBoard.java
@@ -7,8 +7,13 @@ public class GameBoard {
     private final String[] rowNumbers = {" ", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
     private final String[] columnLetters = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
     private String[][] gameBoard = new String[10][10];
-
+    private final String[] gamePieceChoices = {"Aircraft Carrier", "Battleship", "Submarine", "Cruiser", "Destroyer"};
+    private final String[] gamePieceChoicesLengths = {"(5 cells)", "(4 cells)", "(3 cells)", "(3 cells)", "(2 cells)"};
     private GamePiece aircraftCarrier;
+    private GamePiece battleship;
+    private GamePiece submarine;
+    private GamePiece cruiser;
+    private GamePiece destroyer;
 
     public GameBoard() {
         // initialize empty game board
@@ -44,7 +49,7 @@ public class GameBoard {
             System.out.print(columnLetters[i]);
             System.out.print(" ");
             for (int j = 0; j < getGameBoard()[i].length; j++) {
-                System.out.print(gameBoard[i][j]);
+                System.out.print(getGameBoard()[i][j]);
                 if (j != getGameBoard()[i].length - 1) {
                     System.out.print(" ");
                 } else {
@@ -54,7 +59,7 @@ public class GameBoard {
         }
     }
 
-    // ***** AIRCRAFT CARRIER *****
+    // ***** GAME PIECES *****
     public GamePiece getAircraftCarrier() {
         return aircraftCarrier;
     }
@@ -63,45 +68,116 @@ public class GameBoard {
         this.aircraftCarrier = aircraftCarrier;
     }
 
+    public GamePiece getBattleship() {
+        return battleship;
+    }
+
+    public void setBattleship(GamePiece battleship) {
+        this.battleship = battleship;
+    }
+
+    public GamePiece getSubmarine() {
+        return submarine;
+    }
+
+    public void setSubmarine(GamePiece submarine) {
+        this.submarine = submarine;
+    }
+
+    public GamePiece getCruiser() {
+        return cruiser;
+    }
+
+    public void setCruiser(GamePiece cruiser) {
+        this.cruiser = cruiser;
+    }
+
+    public GamePiece getDestroyer() {
+        return destroyer;
+    }
+
+    public void setDestroyer(GamePiece destroyer) {
+        this.destroyer = destroyer;
+    }
+
     // ***** GET COORDINATES *****
 
     public void getCoordinates() {
 
-        Scanner scanner = new Scanner(System.in);
+        int currentGamePieceIndex = 0;
+        boolean allCoordinatesReceived = false;
 
-        System.out.println("Enter the coordinates of the Aircraft Carrier (5 cells):");
+        while (!allCoordinatesReceived) {
 
-        String[] aircraftCarrierStart = scanner.next().split("");
-        String[] aircraftCarrierEnd = scanner.next().split("");
+            Scanner scanner = new Scanner(System.in);
 
-        scanner.close();
+            System.out.printf("Enter the coordinates of the %s %s:", gamePieceChoices[currentGamePieceIndex], gamePieceChoicesLengths[currentGamePieceIndex]);
+            System.out.println();
 
-        // translate the coordinates into indices
-        int letterCount = 0;
-        for (String letter : columnLetters) {
-            if (aircraftCarrierStart[0].equalsIgnoreCase(letter)) {
-                aircraftCarrierStart[0] = String.valueOf(letterCount);
+            String[] coordinateStart = scanner.next().split("");
+            String[] coordinateEnd = scanner.next().split("");
+
+            // translate the coordinates into indices
+            int letterCount = 0;
+            for (String letter : columnLetters) {
+                if (coordinateStart[0].equalsIgnoreCase(letter)) {
+                    coordinateStart[0] = String.valueOf(letterCount);
+                }
+                if (coordinateEnd[0].equalsIgnoreCase(letter)) {
+                    coordinateEnd[0] = String.valueOf(letterCount);
+                }
+                letterCount++;
             }
-            if (aircraftCarrierEnd[0].equalsIgnoreCase(letter)) {
-                aircraftCarrierEnd[0] = String.valueOf(letterCount);
+
+            // handle 3 digit coordinate lengths (the number 10)
+
+            if (coordinateStart.length > 2) {
+                coordinateStart[1] = "10";
             }
-            letterCount++;
+            if (coordinateEnd.length > 2) {
+                coordinateEnd[1] = "10";
+
+            }
+
+            // change from strings to integers
+            int[] gamePieceCoordinates = new int[4];
+
+            gamePieceCoordinates[0] = Integer.parseInt(coordinateStart[0]);
+            gamePieceCoordinates[1] = Integer.parseInt(coordinateStart[1]) - 1;
+            gamePieceCoordinates[2] = Integer.parseInt(coordinateEnd[0]);
+            gamePieceCoordinates[3] = Integer.parseInt(coordinateEnd[1]) - 1;
+
+            // ********** VALIDATE GAME PIECES HERE **********
+
+            String[][] updatedGameBoard;
+
+            if (currentGamePieceIndex == 0) {
+                setAircraftCarrier(new GamePiece(gamePieceCoordinates));
+                updatedGameBoard = getAircraftCarrier().setGamePiece(getGameBoard(), gamePieceCoordinates);
+                currentGamePieceIndex++;
+            } else if (currentGamePieceIndex == 1) {
+                setBattleship(new GamePiece(gamePieceCoordinates));
+                updatedGameBoard = getBattleship().setGamePiece(getGameBoard(), gamePieceCoordinates);
+                currentGamePieceIndex++;
+            } else if (currentGamePieceIndex == 2) {
+                setSubmarine(new GamePiece(gamePieceCoordinates));
+                updatedGameBoard = getSubmarine().setGamePiece(getGameBoard(), gamePieceCoordinates);
+                currentGamePieceIndex++;
+            } else if (currentGamePieceIndex == 3) {
+                setCruiser(new GamePiece(gamePieceCoordinates));
+                updatedGameBoard = getCruiser().setGamePiece(getGameBoard(), gamePieceCoordinates);
+                currentGamePieceIndex++;
+            } else {
+                setDestroyer(new GamePiece(gamePieceCoordinates));
+                updatedGameBoard = getDestroyer().setGamePiece(getGameBoard(), gamePieceCoordinates);
+                allCoordinatesReceived = true;
+                scanner.close();
+            }
+
+            setGameBoard(updatedGameBoard);
+
+            printGameBoard();
         }
 
-        // change from strings to integers
-        int[] aircraftCarrierCoordinates = new int[4];
-
-        aircraftCarrierCoordinates[0] = Integer.parseInt(aircraftCarrierStart[0]);
-        aircraftCarrierCoordinates[1] = Integer.parseInt(aircraftCarrierStart[1]);
-        aircraftCarrierCoordinates[2] = Integer.parseInt(aircraftCarrierEnd[0]);
-        aircraftCarrierCoordinates[3] = Integer.parseInt(aircraftCarrierEnd[1]);
-
-        for (int coordinate : aircraftCarrierCoordinates) {
-            System.out.println(coordinate);
-        }
-
-        this.aircraftCarrier = new GamePiece(aircraftCarrierCoordinates);
-
-        setAircraftCarrier(this.aircraftCarrier);
     }
 }
--- Battleship/Battleship/task/src/battleship/GamePiece.java
+++ Battleship/Battleship/task/src/battleship/GamePiece.java
@@ -1,18 +1,46 @@
 package battleship;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class GamePiece {
 
     // pieces will extend this class (all pieces have coordinates)
     // validatePlacement() method with arraylist of all coordinates added to and validated as player updates coordinates
 
-    private final List<Integer> translatedCoordinates = new ArrayList<>();
-    private final int[] gamePieceCoordinates;
-
-
     public GamePiece(int[] gamePieceCoordinates) {
-        this.gamePieceCoordinates = gamePieceCoordinates;
+    }
+
+    public String[][] setGamePiece(String[][] gameBoard, int[] coordinates) {
+
+        int x1 = coordinates[0];
+        int x2 = coordinates[1];
+
+        int y1 = coordinates[2];
+        int y2 = coordinates[3];
+
+        for (int i = 0; i < gameBoard.length; i++) {
+            for (int j = 0; j < gameBoard[i].length; j++) {
+                if (x1 == y1 && i == x1) { // x is equal
+                    if (x2 < y2) { // if y2 is greater...
+                        if (j >= x2 && j <= y2) { // j must be less than y2
+                            gameBoard[i][j] = "0";
+                        }
+                    } else {
+                        if (j <= x2 && j >= y2) {
+                            gameBoard[i][j] = "0";
+                        }
+                    }
+                } else if (x2 == y2 && j == x2) { // y is equal
+                    if (x1 > y1) { // if x is greater...
+                        if (i <= x1 && i >= y1) { // i must be less than x1
+                            gameBoard[i][j] = "0";
+                        }
+                    } else {
+                        if (i >= x1 && i <= y1) {
+                            gameBoard[i][j] = "0";
+                        }
+                    }
+                }
+            }
+        }
+        return gameBoard;
     }
 }
