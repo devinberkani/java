f0a6965416eb00a0258d73d0a4496482e5fa641a
Devin Berkani <devinberkani@gmail.com>
2022-07-31 20:45:47 -0400

    update exception handling logic

--- Tic-Tac-Toe with AI/Tic-Tac-Toe with AI/task/src/tictactoe/GameBoard.java
+++ Tic-Tac-Toe with AI/Tic-Tac-Toe with AI/task/src/tictactoe/GameBoard.java
@@ -1,18 +1,24 @@
 package tictactoe;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class GameBoard {
-
     private final static Scanner input = new Scanner(System.in);
-    private String[][] gameBoard = new String[3][3];
 
-    private String[][] testGameBoard = new String[3][3];
+    private boolean isGameOver = false;
+    private String[][] gameBoard = new String[3][3];
 
     private int[] validUserCoordinates = new int[2];
 
+    private final String[] gamePieces = {"X", "O"};
+    private int currentGamePieceIndex = 0; //USE ENUM FOR THIS?
+
     public GameBoard() {
         getUserCells();
+        getUserCoordinates();
+        printGameBoard();
+        switchGamePieceIndex();
     }
 
     public void printGameBoard() {
@@ -37,6 +43,33 @@ public class GameBoard {
         System.out.println(topBottomBorder);
     }
 
+    public String[][] testCoordinates(int[] validUserCoordinates) {
+
+        String[][] newGameBoard = new String[3][3];
+
+        for (int i = 0; i < getGameBoard().length; i++) {
+            for (int j = 0; j < getGameBoard()[i].length; j++) {
+                newGameBoard[i][j] = getGameBoard()[i][j];
+            }
+        }
+
+        newGameBoard[validUserCoordinates[0]][validUserCoordinates[1]] = gamePieces[getCurrentGamePieceIndex()];
+
+        return newGameBoard;
+
+    }
+
+    public void switchGamePieceIndex() {
+
+        if (getCurrentGamePieceIndex() == 0) {
+            setCurrentGamePieceIndex(1);
+        }
+        if (getCurrentGamePieceIndex() == 1) {
+            setCurrentGamePieceIndex(0);
+        }
+
+    }
+
     // ***** GET USER COORDINATES *****
 
     public void getUserCoordinates() {
@@ -48,29 +81,45 @@ public class GameBoard {
             System.out.print("Enter the coordinates: ");
 
             // validate user coordinates
+
             isValid = isValidInput();
 
         }
 
     }
 
-    public boolean isValidInput() throws Exception {
+    public boolean isValidInput() {
+
+        String[][] newGameBoard = new String[3][3];
 
         // YOU ARE HERE
-
         try {
-            for (int coordinate : getValidUserCoordinates()) {
-                // check that the coordinate is a number
-                coordinate = input.nextInt();
-                // check that the coordinate is from 1 to 3
-                if (coordinate < 1 || coordinate > 3) {
-                    throw new Exception("");
-                }
+            for (int i = 0; i < getValidUserCoordinates().length; i++) {
+                getValidUserCoordinates()[i] = input.nextInt() - 1;
             }
-        } catch (Exception e) {
-            
-        }
 
+            newGameBoard = testCoordinates(getValidUserCoordinates());
+
+            String coordinate = getGameBoard()[getValidUserCoordinates()[0]][getValidUserCoordinates()[1]];
+
+            if (coordinate.equals("X") || coordinate.equals("O")) {
+                throw new CellOccupiedException("This cell is occupied! Choose another one!");
+            } else {
+                setGameBoard(newGameBoard);
+                return true;
+            }
+
+        } catch (CellOccupiedException cellOccupiedException) {
+            System.out.println(cellOccupiedException.getMessage());
+        // check that coordinates are numbers
+        } catch (InputMismatchException inputMismatchException) {
+            System.out.println("You should enter numbers!");
+            input.nextLine(); // consume input
+        // check that coordinates are within correct range
+        } catch (ArrayIndexOutOfBoundsException arrayIndexOutOfBoundsException) {
+            System.out.println("Coordinates should be from 1 to 3!");
+        }
+        return false;
     }
 
     // ***** GET USER CELLS AND ADD THEM TO GAME BOARD *****
@@ -93,20 +142,22 @@ public class GameBoard {
 
         // add user input to test game board
 
+        String[][] newGameBoard = new String[3][3];
+
         int index = 0;
-        for (int i = 0; i < getTestGameBoard().length; i++) {
-            for (int j = 0; j < getTestGameBoard()[i].length; j++) {
+        for (int i = 0; i < newGameBoard.length; i++) {
+            for (int j = 0; j < newGameBoard[i].length; j++) {
                 // if one of the cells is "_" it should be printed as an empty space
                 if (userInput[index].equals("_")) {
-                    getTestGameBoard()[i][j] = " ";
+                    newGameBoard[i][j] = " ";
                 } else {
-                    getTestGameBoard()[i][j] = userInput[index];
+                    newGameBoard[i][j] = userInput[index];
                 }
                 index++;
             }
         }
 
-        return getTestGameBoard();
+        return newGameBoard;
     }
 
     // ***** GETTERS AND SETTERS *****
@@ -119,19 +170,27 @@ public class GameBoard {
         this.gameBoard = gameBoard;
     }
 
-    public String[][] getTestGameBoard() {
-        return testGameBoard;
-    }
-
-    public void setTestGameBoard(String[][] testGameBoard) {
-        this.testGameBoard = testGameBoard;
-    }
-
     public int[] getValidUserCoordinates() {
         return validUserCoordinates;
     }
 
     public void setValidUserCoordinates(int[] validUserCoordinates) {
         this.validUserCoordinates = validUserCoordinates;
+    }
+
+    public int getCurrentGamePieceIndex() {
+        return currentGamePieceIndex;
+    }
+
+    public void setCurrentGamePieceIndex(int currentGamePieceIndex) {
+        this.currentGamePieceIndex = currentGamePieceIndex;
+    }
+
+    public boolean isGameOver() {
+        return isGameOver;
+    }
+
+    public void setGameOver(boolean gameOver) {
+        isGameOver = gameOver;
     }
 }
