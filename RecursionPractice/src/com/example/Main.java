4915919ac9e638d1ea23abcaeaca9f047fa12673
Devin Berkani <devinberkani@gmail.com>
2022-08-14 09:57:05 -0400

    solve problem of the day

--- RecursionPractice/src/com/example/Main.java
+++ RecursionPractice/src/com/example/Main.java
@@ -1,19 +1,35 @@
 package com.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        printNumber(1);
+
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        while (n != -1) {
+            collatz(n, 0);
+            n = scanner.nextInt();
+        }
     }
 
     // 1. what's the simples possible input?
 
     // example 1 from video
-    public static void printNumber(int n) {
-        if (n > 5) {
-            return;
+    public static int collatz(int n, int counter) {
+
+        if (n == 0 || n == 1) {
+            System.out.println(counter);
+            return 1;
         }
-        System.out.println(n);
-        printNumber(n + 1);
+        if (n % 2 == 0) {
+            counter++;
+            return collatz(n / 2, counter);
+        } else {
+            counter++;
+            return collatz((3 * n) + 1, counter);
+        }
 
     }
 }
