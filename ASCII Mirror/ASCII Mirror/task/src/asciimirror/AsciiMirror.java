7484eaf93f38192b8c26c66a95d80de0431e7506
Devin Berkani <devinberkani@gmail.com>
2022-08-18 08:07:47 -0400

    complete stage two of ascii friends project

--- ASCII Mirror/ASCII Mirror/task/src/asciimirror/AsciiMirror.java
+++ ASCII Mirror/ASCII Mirror/task/src/asciimirror/AsciiMirror.java
@@ -1,14 +1,25 @@
 package asciimirror;
 
+import java.util.Scanner;
+
 public class AsciiMirror {
+    final static Scanner scanner = new Scanner(System.in);
+
+    protected void repeatFilePath(String filePath) {
+        System.out.println(filePath);
+    }
+    protected String getFilePath() {
+        System.out.println("Input the file path:");
+        String filePath = scanner.nextLine();
+        repeatFilePath(filePath);
+        return filePath;
+    }
+
     protected void printCow() {
-        System.out.println("                    _______ \n" +
-                "                   < hello >\n" +
-                "                    ------- \n" +
-                "            ^__^   /        \n" +
-                "    _______/(oo)  /         \n" +
-                "/\\/(       /(__)            \n" +
-                "   | w----||                \n" +
-                "   ||     ||                ");
+        System.out.println("            ^__^\n" +
+                "    _______/(oo)\n" +
+                "/\\/(       /(__)\n" +
+                "   | w----||    \n" +
+                "   ||     ||    ");
     }
 }
--- ASCII Mirror/ASCII Mirror/task/src/asciimirror/Main.java
+++ ASCII Mirror/ASCII Mirror/task/src/asciimirror/Main.java
@@ -3,6 +3,7 @@ package asciimirror;
 public class Main {
     public static void main(String[] args) {
         AsciiMirror asciiMirror = new AsciiMirror();
+        asciiMirror.getFilePath();
         asciiMirror.printCow();
     }
 }
