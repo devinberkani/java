9a80c7a623a1b977a0b6c105998a70461c7097e1
Devin Berkani <devinberkani@gmail.com>
2022-09-12 15:35:07 -0400

    complete stage 4 of flashcards project

--- Flashcards/Flashcards/task/src/flashcards/Deck.java
+++ Flashcards/Flashcards/task/src/flashcards/Deck.java
@@ -1,9 +1,12 @@
 package flashcards;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 
 public class Deck {
+    // old list for having an arraylist of flashcard objects
     private ArrayList<Flashcard> deck = new ArrayList<>();
+    private LinkedHashMap<String, String> hashMapDeck = new LinkedHashMap<>();
     public Deck() {
 
     }
@@ -16,5 +19,13 @@ public class Deck {
 
     private void setDeck(ArrayList<Flashcard> deck) {
         this.deck = deck;
+    }
+
+    protected LinkedHashMap<String, String> getHashMapDeck() {
+        return hashMapDeck;
+    }
+
+    protected void setHashMapDeck(LinkedHashMap<String, String> hashMapDeck) {
+        this.hashMapDeck = hashMapDeck;
     }
 }
--- Flashcards/Flashcards/task/src/flashcards/Menu.java
+++ Flashcards/Flashcards/task/src/flashcards/Menu.java
@@ -11,17 +11,41 @@ public class Menu {
 
     protected void goThroughDeck(Deck deck) {
 
-        for (int i = 0; i < getDeckSize(); i++) {
-            String front = deck.getDeck().get(i).getFront();
-            String back = deck.getDeck().get(i).getBack();
+        String incorrectMessage = "Wrong. The right answer is \"";
+
+        for (var flashcard : deck.getHashMapDeck().entrySet()) {
+            String front = flashcard.getKey();
+            String back = deck.getHashMapDeck().get(front);
             System.out.println("Print the definition of \"" + front + "\":");
             String answer = scanner.nextLine();
             if (answer.equals(back)) {
                 System.out.println("Correct!");
+            } else if (deck.getHashMapDeck().containsValue(answer)) {
+                // if the user entered the answer for a different card, show them the one they had the right answer for
+                String correctTerm = "";
+                for (var flashcard2 : deck.getHashMapDeck().entrySet()) {
+                    if (flashcard2.getValue().equals(answer)) {
+                        correctTerm = flashcard2.getKey();
+                    }
+                }
+                System.out.println(incorrectMessage + back + "\", but your definition is correct for \"" + correctTerm + "\".");
             } else {
-                System.out.println("Wrong. The right answer is \"" + back + "\".");
+                System.out.println(incorrectMessage + back + "\".");
             }
         }
+
+        // old logic for having an arraylist of flashcard objects
+//        for (int i = 0; i < getDeckSize(); i++) {
+//            String front = deck.getDeck().get(i).getFront();
+//            String back = deck.getDeck().get(i).getBack();
+//            System.out.println("Print the definition of \"" + front + "\":");
+//            String answer = scanner.nextLine();
+//            if (answer.equals(back)) {
+//                System.out.println("Correct!");
+//            } else {
+//                System.out.println("Wrong. The right answer is \"" + back + "\".");
+//            }
+//        }
     }
 
     protected void fillDeck(Deck deck) {
@@ -30,13 +54,33 @@ public class Menu {
         for (int i = 1; i <= getDeckSize(); i++) {
             System.out.println("Card #" + i + ":");
             String front = scanner.nextLine();
+
+            // check if front is already in deck
+            while (deck.getHashMapDeck().containsKey(front)) {
+                System.out.println("The term \"" + front + "\" already exists. Try again:");
+                front = scanner.nextLine();
+            }
+
             System.out.println("The definition for card #" + i + ":");
             String back = scanner.nextLine();
-            addFlashcardToDeck(deck, i, front, back);
+
+            // check if back is already in deck
+            while (deck.getHashMapDeck().containsValue(back)) {
+                System.out.println("The definition \"" + back + "\" already exists. Try again:");
+                back = scanner.nextLine();
+            }
+
+            addFlashcardToHashMapDeck(deck, front, back);
+//            addFlashcardToDeck(deck, i, front, back); // old logic for having an arraylist of flashcards
         }
 
     }
 
+    private void addFlashcardToHashMapDeck(Deck deck, String front, String back) {
+        deck.getHashMapDeck().put(front, back);
+    }
+
+    // old logic for having an arraylist of flashcard objects
     private void addFlashcardToDeck(Deck deck, int number, String front, String back) {
         Flashcard flashcard = new Flashcard(number, front, back);
         deck.getDeck().add(flashcard);
